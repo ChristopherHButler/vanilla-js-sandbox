c2aac8e1658a26071664ebfc1e0a4757fa5e7511
Christopher Butler <christopher.harold.butler@gmail.com>
2020-05-20 18:47:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sandbox
+---
+This is a minimal, vanilla javascript sandbox setup for writing javascript (not react) code.
+
+
+fire it up with
+
+```
+> npm run start
+```
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build'),
+    compress: true,
+    port: 9000,
+    hot: true,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  }
+};
